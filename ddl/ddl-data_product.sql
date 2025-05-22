30fc5cc9533c5a6de1a5027b4f6aaebefce48573
gguglielmoni <gilles.guglielmoni1@decathlon.com>
2025-05-22 19:23:25 +0200

    fix typo

--- ddl/ddl-data_product.sql
+++ ddl/ddl-data_product.sql
@@ -3,7 +3,7 @@ drop table if exists data_product;
 create table data_product (
     id VARCHAR,
     description VARCHAR,
-    contract_id VARCHAR,
+    contact_id VARCHAR,
     created_at_utc TIMESTAMP,
     updated_at_utc TIMESTAMP
 );
